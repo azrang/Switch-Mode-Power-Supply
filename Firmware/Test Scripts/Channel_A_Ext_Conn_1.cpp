e1930f4b1510b4d4e12caa63e13cee6c207e8ab7
Ilona Lameka <91708567+ilona403@users.noreply.github.com>
2025-04-02 18:38:36 -0400

    Formatting

--- Firmware/Test Scripts/Channel_A_Ext_Conn_1.cpp
+++ Firmware/Test Scripts/Channel_A_Ext_Conn_1.cpp
@@ -11,13 +11,15 @@ int lcdColumns = 16;
 int lcdRows = 2;
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows); 
 
-void setup() {
+void setup() 
+{
   lcd.init();                  
   lcd.backlight();
   pinMode(A_BUTT, INPUT);
 }
 
-void loop() {
+void loop() 
+{
   if (!digitalRead(A_BUTT))
   {
     lcd.setCursor(0, 0);
