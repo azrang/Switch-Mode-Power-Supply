cd266a6dcf1a9a1e7cd5411b5d6b4d4e08c7f76a
Ilona Lameka <91708567+ilona403@users.noreply.github.com>
2025-04-07 13:38:05 -0400

    Update Channel_A_ESP32_PWM.cpp

--- Firmware/Test Scripts/Channel_A_ESP32_PWM.cpp
+++ Firmware/Test Scripts/Channel_A_ESP32_PWM.cpp
@@ -8,17 +8,11 @@
 
 const int A_BUTT = 23; // MCU_A_GPIO23
 const int BUCK = 25; // MCU_A_GPIO25
-int lcdColumns = 16;
-int lcdRows = 2;
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows); 
 
 int PWM = 102; // 10% Duty Cycle
-int prevStateA = 0;
 
 void setup() 
 {
-  lcd.init();                  
-  lcd.backlight();
   pinMode(A_BUTT, INPUT);
   ledcSetup(0, 10000, 10); // 10kHz, 10 bit resolution
   ledcAttachPin(BUCK, 0);
@@ -26,18 +20,5 @@ void setup()
 
 void loop() 
 {
-  if (digitalRead(A_BUTT) && !prevStateA)
-  {
     ledcWrite(0, PWM);
-    lcd.setCursor(0, 0);
-    lcd.print(String("PWM: ") + String(PWM) + String("  "));
-    prevStateA = 1;
-  }
-  else if(!digitalRead(A_BUTT))
-  {
-    ledcWrite(0, 0);
-    lcd.setCursor(0, 0);
-    lcd.print("PWM 0      ");
-    prevStateA = 0;
-  }
 }
