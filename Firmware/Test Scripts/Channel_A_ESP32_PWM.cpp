d2e45c3913b6fdf003beb5b6db7e81948f29974b
Ilona Lameka <91708567+ilona403@users.noreply.github.com>
2025-04-07 13:57:21 -0400

    Update Channel_A_ESP32_PWM.cpp

--- Firmware/Test Scripts/Channel_A_ESP32_PWM.cpp
+++ Firmware/Test Scripts/Channel_A_ESP32_PWM.cpp
@@ -6,14 +6,12 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 
-const int A_BUTT = 23; // MCU_A_GPIO23
 const int BUCK = 25; // MCU_A_GPIO25
 
 int PWM = 102; // 10% Duty Cycle
 
 void setup() 
 {
-  pinMode(A_BUTT, INPUT);
   ledcSetup(0, 10000, 10); // 10kHz, 10 bit resolution
   ledcAttachPin(BUCK, 0);
 }
