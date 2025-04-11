ec6983c08eae0cd8fabbf802a908747cdeb3d1e6
Ilona Lameka <91708567+ilona403@users.noreply.github.com>
2025-04-11 14:07:37 -0400

    Create Channel_A_Transformer_Gate.cpp

---
+++ Firmware/Test Scripts/Channel_A_Transformer_Gate.cpp
@@ -0,0 +1,44 @@
+// MCU Testing Script - Channel A Transformers
+// Channel_A_Transformer_Gate.cpp
+// Switch Mode Power Supply - Senior Projects
+// Azra Rangwala, Ilona Lameka, Ridwan Hussain
+
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+
+const int A_BUTT = 23; // MCU_A_GPIO23
+const int DRIVE = 4; // MCU_A_GPIO4
+int lcdColumns = 16;
+int lcdRows = 2;
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows); 
+
+int PWM = 256; // 25% Duty Cycle
+int prevStateA = 0;
+
+void setup() 
+{
+  lcd.init();                  
+  lcd.backlight();
+  pinMode(A_BUTT, INPUT);
+  ledcSetup(0, 50000, 10); // 50kHz
+  ledcAttachPin(DRIVE, 0);
+}
+
+void loop() 
+{
+  if (digitalRead(A_BUTT) && !prevStateA)
+  {
+    ledcWrite(0, PWM);
+    lcd.setCursor(0, 0);
+    lcd.print(String("PWM: ") + String(PWM) + String("  "));
+    prevStateA = 1;
+  }
+  else if(!digitalRead(A_BUTT))
+  {
+    ledcWrite(0, 0);
+    lcd.setCursor(0, 0);
+    lcd.print("PWM 0      ");
+    prevStateA = 0;
+  }
+  delay(100);
+}
