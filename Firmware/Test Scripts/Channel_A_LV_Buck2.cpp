f0806b7a6f97053095a8fbb7c075202efeabe543
Ilona Lameka <91708567+ilona403@users.noreply.github.com>
2025-04-22 17:32:43 -0400

    Update Channel_A_LV_Buck2.cpp

--- Firmware/Test Scripts/Channel_A_LV_Buck2.cpp
+++ Firmware/Test Scripts/Channel_A_LV_Buck2.cpp
@@ -1,5 +1,5 @@
 // MCU Testing Script - Channel A HV/LV
-// Channel_A_LV_Buck2.cpp
+// Channel_A_LV_Buck1.cpp
 // Switch Mode Power Supply - Senior Projects
 // Azra Rangwala, Ilona Lameka, Ridwan Hussain
 
@@ -9,53 +9,83 @@
 const int A_BUTT = 23; // MCU_A_GPIO23
 const int HV_LV_OUT = 18; // MCU_A_GPIO18
 const int BUCK = 25; // MCU_A_GPIO25
+const int DRIVE = 4; // MCU_A_GPIO4
 int lcdColumns = 16;
 int lcdRows = 2;
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows); 
-
 int prevState = 0;
-int delayTime = 100;
+
+
 
 void setup() 
 {
-  //Serial.begin (115200);
-  //Serial.println("MCU Restarting...");
+  Serial.begin (115200);
+  Serial.println("MCU Restarting...");
   lcd.init();                  
   lcd.backlight();
   pinMode(A_BUTT, INPUT);
   pinMode(HV_LV_OUT, OUTPUT);
-  ledcSetup(0, 10000, 10); // Play around with frequency and resolution
+  ledcSetup(0, 35000, 10); 
   ledcAttachPin(BUCK, 0);
+
+  ledcSetup(1, 42000, 10); 
+  ledcAttachPin(DRIVE, 1);
+
   digitalWrite(HV_LV_OUT, 1);
 }
 
 void loop() 
 {
-    int PWM = 102; // 10% starting point
-    if (digitalRead(A_BUTT) && !prevState)
+  if (digitalRead(A_BUTT) && !prevState)
+  {
+    int drive_PWM = 102;
+    int buck_PWM = 100;
+    for(int i = 0; (i <= 19 && digitalRead(A_BUTT)); i++)
     {
-      digitalWrite(HV_LV_OUT, 0);
-      for(int i = 0; (i <= 31 && digitalRead(A_BUTT)); i++)
-      {
-        lcd.setCursor(0, 0);
-        lcd.print(String("PWM: ") + String(PWM) + String("              "));
-        lcd.setCursor(0, 1);
-        lcd.print(String("I: ") + String(i) + String("                   "));
-        ledcWrite(0, PWM);
-        //Serial.println(PWM);
-        PWM += 10; //Math to increase by voltage
-        delay(delayTime);     
-      }
-      prevState = 1;
-    }
-    else if(!digitalRead(A_BUTT))
-    {
-      digitalWrite(HV_LV_OUT, 1);
-      ledcWrite(0, 0);
+      
       lcd.setCursor(0, 0);
-      lcd.print("PWM 0      ");
-      //Serial.println(String(0));
-      prevState = 0;
+      lcd.print(String("Drive PWM: ") + String(drive_PWM) + String("              "));
+      lcd.setCursor(0, 1);
+      lcd.print(String("I: ") + String(i) + String("                   "));
+      ledcWrite(1, drive_PWM);
+      Serial.println(drive_PWM);
+      drive_PWM += 10; //Math to increase by voltage
+      delay(100); 
     }
-    delay(100);
+    ledcWrite(1, drive_PWM-5); //297 is 12V
+    delay(10);
+    lcd.setCursor(0, 0);
+    lcd.print(String(drive_PWM-5) + String("                   "));
+    delay(1000);
+    
+    //Serial.println(PWM);
+    digitalWrite(HV_LV_OUT, 0);
+    lcd.setCursor(0, 0);
+    lcd.print("Buck                          ");
+    lcd.setCursor(0, 1);
+    lcd.print("                             ");
+
+    for(int k = 0; (k <= 81 && digitalRead(A_BUTT)); k++)
+    {
+      lcd.setCursor(0, 0);
+      lcd.print(String("Buck PWM: ") + String(buck_PWM) + String("              "));
+      lcd.setCursor(0, 1);
+      lcd.print(String("K: ") + String(k) + String("                   "));
+      ledcWrite(0, buck_PWM);
+      Serial.println(buck_PWM);
+      buck_PWM += 10; //Math to increase by voltage
+      delay(100);
+    }
+    prevState = 1;
+  }
+  else if (!digitalRead(A_BUTT))
+  {
+    ledcWrite(1, 0);
+    digitalWrite(HV_LV_OUT, 1);
+    ledcWrite(0, 0);
+    lcd.setCursor(0, 0);
+    lcd.print("No PWM                    ");
+    prevState = 0;
+  }
+  delay(100);
 }
