a9f33f998e6c646d903f74084e8674de79925dca
Ilona Lameka <91708567+ilona403@users.noreply.github.com>
2025-04-28 21:55:37 -0400

    Update Channel_B_Ext_Conn_1.cpp

--- Firmware/Test Scripts/Channel_B_Ext_Conn_1.cpp
+++ Firmware/Test Scripts/Channel_B_Ext_Conn_1.cpp
@@ -6,7 +6,7 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 
-const int B_BUTT = 23; // MCU_A_GPIO23
+const int B_BUTT = 23; // MCU_B_GPIO23
 int lcdColumns = 16;
 int lcdRows = 2;
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows); 
