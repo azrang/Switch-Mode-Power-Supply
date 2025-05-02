8ba1167e1d9ea4a2a7f498bda572da4dbecc6f32
Ilona Lameka <91708567+ilona403@users.noreply.github.com>
2025-05-02 12:29:32 -0400

    Update Channel_A_Ext_Conn2.cpp

--- Firmware/Test Scripts/Channel_A_Ext_Conn2.cpp
+++ Firmware/Test Scripts/Channel_A_Ext_Conn2.cpp
@@ -7,8 +7,11 @@
 #include <LiquidCrystal_I2C.h>
 
 const int A_BUTT = 23; // MCU_A_GPIO23
-const int HV_LV_OUT = 18; // MCU_A_GPIO18
-const int OUT_ENABLE = 26; // MCU_A_GPIO26
+const int V_KNOB  = 39; // MCU_A_ADC1_C3
+const int C_KNOB = 34; // MCU_A_ADC1_C6
+
+float gbl_vPotCalc, gbl_cPotCalc; 
+
 int lcdColumns = 16;
 int lcdRows = 2;
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows); 
@@ -16,31 +19,45 @@ int prevState = 0;
 
 void setup() 
 {
-  //Serial.begin (115200);
-  //Serial.println("MCU Restarting...");
   lcd.init();                  
   lcd.backlight();
   pinMode(A_BUTT, INPUT);
-  pinMode(HV_LV_OUT, OUTPUT);
-  pinMode(OUT_ENABLE, OUTPUT);
-  digitalWrite(HV_LV_OUT, 1);
 }
 
 void loop() 
 {
-  if (digitalRead(A_BUTT) && !prevState)
-  {
-    digitalWrite(OUT_ENABLE, 1);
-    lcd.setCursor(0, 0);
-    lcd.print("Output Enabled                   ");
-    prevState = 1;
-  }
-  else if (!digitalRead(A_BUTT))
-  {
-    digitalWrite(OUT_ENABLE, 0);
-    lcd.setCursor(0, 0);
-    lcd.print("No Output                    ");
-    prevState = 0;
-  }
+  readKnobs();
   delay(100);
+  lcdScreen(gbl_vPotCalc, gbl_cPotCalc);
+}
+
+void readKnobs()
+{
+  // Getting the ADC Knob Values
+  int vPotTotal = 0;
+  int cPotTotal = 0;
+  for (int i = 0; i < 100; i++) // Getting muliple samples due to ADC fluctuations
+  {
+    vPotTotal += analogRead(V_KNOB);
+    cPotTotal += analogRead(C_KNOB);
+    vTaskDelay(pdMS_TO_TICKS(1));
+  }
+  int vPot = vPotTotal / 100; 
+  gbl_vPotCalc = ((vPot / 4095.0) * 19.0) + 1;
+
+  int cPot = cPotTotal / 100;
+  gbl_cPotCalc = (cPot / 4095.0) * 3.0;
+}
+
+void lcdScreen(float v, float c) 
+{   
+    lcd.setCursor(0, 0);
+    char vDisplay[6];  
+    dtostrf(v, 4, 2, vDisplay); 
+    lcd.print(String("Voltage: ") + String(vDisplay) + String(" V       "));
+
+    lcd.setCursor(0,1);
+    char cDisplay[6];  
+    dtostrf(c, 4, 2, cDisplay); 
+    lcd.print(String("Current: ") + String(cDisplay) + String(" A"));
 }
